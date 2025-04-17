1a590b060ebe84104ae58da62f858651f83ce346
Leonardo Cella <leeoceella@gmail.com>
2025-04-16 21:49:39 -0300

    § Dockerfile foi atualizado; § app foi atualizado

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["python", "app.py"]
+# CMD ["python", "app.py"]
+CMD ["sh", "-c", "python app.py > /var/log/flask_stdout.log 2>&1"]
--- app.py
+++ app.py
@@ -13,5 +13,11 @@ def hello():
     app.logger.info("Accessed '/' route")
     return "Hello, World!"
 
+
+@app.route("/about")
+def about():
+	  app.logger.info("Accessed '/about' route")
+        return "Ola, voce acessou a pagina sobre: <br> Eu sou o dev <br> "
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
